66a38ab6703cbf146cddd52e751a18de504c8858
Mikito Takada <firstname.surname@gmail.com>
2011-11-16 18:50:33 -0800

    Add Socket.io 0.8.7 test, change packages to isolate each bench dependency in its own subdirectory

---
+++ bench/sio087/client.js
@@ -0,0 +1,23 @@
+
+// SOCKET IO bench
+var io = require('socket.io-client');
+
+var opts = {
+  transports: ['websocket']
+};
+
+function createClient(index, controller) {
+  var client = io.connect('http://localhost:8000', opts);
+  client.on('connect', function(){ controller.clientConnect(index); });
+  client.on('message', function(){ controller.clientMessage(index); });
+  client.on('disconnect', function(){ controller.clientDisconnect(index); });
+
+  return client;
+}
+
+// some libraries use send, others use write etc.
+function sendMessage(client, str) {
+  client.send(str);
+}
+
+module.exports = { createClient: createClient, sendMessage: sendMessage };
